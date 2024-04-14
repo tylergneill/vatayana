742fecf2c4425ba7bc9c8287eae1d08c6c234364
tylergneill <tyler.g.neill@gmail.com>
2024-04-13 22:41:18 -0400

    chore: generalize mongo server

--- flask_app.py
+++ flask_app.py
@@ -9,7 +9,8 @@ import IR_tools
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["SECRET_KEY"] = "safaksdfakjdshfkajshfka" # for session, no actual need for secrecy
-app.config["MONGO_URI"] = "mongodb://localhost:27017/vatayana"
+DB_SERVER = os.getenv("DB_SERVER", "localhost")
+app.config["MONGO_URI"] = f"mongodb://{DB_SERVER}:27017/vatayana"
 # MONGO_CRED = open('mongo_cred/mongo_cred.txt').read().strip()
 # app.config["MONGO_URI"] = f"mongodb+srv://tyler:{MONGO_CRED}@sanskrit2.urlv9ek.mongodb.net/vatayana?retryWrites=true&w=majority"
 
