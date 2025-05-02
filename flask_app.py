48544dc48d7a8bafa638ae5391890ccd5ff1bb67
tylergneill <tyler.g.neill@gmail.com>
2025-05-02 11:28:31 -0400

    chore (scout): add main block to run app locally

--- flask_app.py
+++ flask_app.py
@@ -505,3 +505,6 @@ def search_depth():
                             page_subtitle="searchDepth",
                             searchDepthInner_HTML=searchDepthInner_HTML
                             )
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5020)
